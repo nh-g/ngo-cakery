587f6c11f0f68a039dc71d774fa1e85f1d97fe32
nh-g <giangngohong@gmail.com>
2021-04-01 20:11:06 +0200

    added component to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,20 @@
-import ImageComponent from "../components/ImageComponent";
-import homeData from "../assets/data/homeCake";
-export default function Home(){
-    const homeArray = homeData.map((item) => {return (
-    <ImageComponent key={item.id} title={item.title} filePath={item.filePath} />
-    );
-    });
+import homeData from '../assets/data/homeCake';
+import ImageComponent from '../components/ImageComponent';
+
+import "../assets/scss/_home.scss";
+export default function Home() {
+  const homeArray = homeData.map((item) => {
     return (
-      <div className="home container">
-        <div className="grid">{homeArray}</div>
-      </div>
+      <ImageComponent
+        key={item.id}
+        title={item.title}
+        filePath={item.filePath}
+      />
     );
+  });
+  return (
+    <div className="home container">
+      <div className="grid">{homeArray}</div>
+    </div>
+  );
 }
