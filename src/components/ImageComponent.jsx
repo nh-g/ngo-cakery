c04fe654ea942d6b368ceef13ee5df29950cfb76
nh-g <giangngohong@gmail.com>
2021-04-02 22:32:03 +0200

    add title price to product p by modify ImageCompo

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,10 +1,14 @@
-export default function ImageComponent({ title, filePath }) {
+export default function ImageComponent({ title, price, filePath }) {
   const [fileFolder, fileName] = filePath.split("/");
   const imageURL = require(`../assets/images/${fileFolder}/${fileName}`)
     .default;
   return (
     <div className="image-grid">
       <img src={imageURL} alt={title} style={{ width: "100%" }} />
+      <figcaption>
+        <p>{title}</p>
+        <p>{price}</p>
+      </figcaption>
     </div>
   );
 }
