78f9766db024cb15ba46ed7efe536ecd647f1076
nh-g <giangngohong@gmail.com>
2021-04-12 03:01:56 +0200

    link logo to home page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -24,24 +24,27 @@ export default function Navigation() {
     const [menuActive, setMenuActive] = useState(false)
     return (
       <div className="header">
-        <img
-          className="brand "
-          src={require("../assets/images/logo/cupcakelogo.jpeg").default}
-          alt="Logo"
-        />
+        <Link to="/">
+          <img
+            className="brand "
+            src={require("../assets/images/logo/cupcakelogo.jpeg").default}
+            alt="Logo"
+          />
+        </Link>
         <div className={`nav-list ${menuActive && "active"}`}>
           <ul>
             {navLinks.map((link, index) => (
               <li key={index}>
-                <Link to={link.path} onClick={() => setMenuActive(!menuActive)}>{link.title}</Link>
+                <Link to={link.path} onClick={() => setMenuActive(!menuActive)}>
+                  {link.title}
+                </Link>
               </li>
             ))}
           </ul>
         </div>
-        <div className ="menu"  onClick={() => setMenuActive(!menuActive)} >
-            <div className="bar"></div>
+        <div className="menu" onClick={() => setMenuActive(!menuActive)}>
+          <div className="bar"></div>
         </div>
-                
       </div>
     );
 }
